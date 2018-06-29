7f55a7112aec570ad137dcf6a1b40809bd364917
lolo <jdanov1@yandex.ru>
2018-06-29 20:24:32 +0300

    zz

--- db/migrate/20180622104545_change_first_user.rb
+++
@@ -1,8 +0,0 @@
-class ChangeFirstUser < ActiveRecord::Migration[5.2]
-  def change
-    # Initialize first account:
-    u = User.where("email = 'test@test.com'").first!
-    u.superadmin_role     = true
-    u.save!
-  end
-end
--- db/migrate/20180622105215_change_first_user_avatar.rb
+++
@@ -1,9 +0,0 @@
-class ChangeFirstUserAvatar < ActiveRecord::Migration[5.2]
-  def change
-    # Initialize first account:
-    data = File.read(Rails.root + "tmp/nezna.png")
-    u = User.where("email = 'test@test.com'").first!
-    u.avatar = data
-    u.save!
-  end
-end
